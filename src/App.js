9eac2a0b010984b47e4bad62dde3245832b6f556
Alikoq <aquliyev75@gmail.com>
2022-01-01 23:24:04 +0100

    new code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         I love Edo
+         I love Edo and Bahar
         </p>
         <a
           className="App-link"
