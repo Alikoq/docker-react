23aca89dce15dfa0f90b98423917c675ff51e245
Alikoq <aquliyev75@gmail.com>
2022-01-03 23:35:10 +0100

    change text app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         I love Edo and Bahar -family
+          I love google
         </p>
         <a
           className="App-link"
